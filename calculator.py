6caa91a3eaea1bec7cffd6fed98705d44a3a1091
shubham-chougule <shubhamchougule0106@gmail.com>
2025-07-12 10:24:26 +0530

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,14 @@
+import streamlit as st
+import datetime
+from datetime import date
+st.title("Age calculator")
+name=st.text_input("Enter your name")
+if name:
+    st.write(f"Hey {name}!lets calculate your age")
+dob=st.date_input(
+    "enter your dob",
+                  min_value=datetime.date(1900,1,1),
+                max_value=datetime.date.today())
+today=date.today()
+age=today.year -dob.year -((today.month,today.day) <(dob.month,dob.day))
+st.success(f"your age is {age}")
